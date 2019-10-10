8b1e71d528c208f9ad1577d38fd1acd8a9038f57
IshitaTakeshi <ishitah.takeshi@gmail.com>
2019-10-10 12:56:12 +0900

    Add 'setup.py' and '.travis.yml'

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup(
+    name='sba',
+    description='SBA',
+    url='http://github.com/IshitaTakeshi/SBA',
+    author='Takeshi Ishita',
+    author_email='ishitah.takeshi@gmail.com',
+    license='Apache 2.0',
+    packages=['sba'],
+    install_requires=['numpy'],
+    tests_require=['pytest']
+)
