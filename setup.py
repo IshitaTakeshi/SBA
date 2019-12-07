83a9dd14ab6cb33b4da73a15c8b42c783ad36525
IshitaTakeshi <ishitah.takeshi@gmail.com>
2019-12-08 02:52:45 +0900

    Set version = 0.0.1

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ from setuptools import setup
 setup(
     name='sparseba',
     description='SBA',
+    version="0.0.1",
     url='http://github.com/IshitaTakeshi/SBA',
     author='Takeshi Ishita',
     author_email='ishitah.takeshi@gmail.com',
